41d8becff4929d5078a7f3350f0d4e1dfda0b759
Drew Willard <drew@bcalternative.com>
2024-09-20 14:12:35 -0600

    Commit all changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/hello')
+    fetch('http://localhost:5001/api/hello')
       .then(response => response.json())
       .then(data => setMessage(data.message));
   }, []);
