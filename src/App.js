dbc7094932d1c03a72581259471d0e3b21c648cd
Drew Willard <drew@bcalternative.com>
2024-09-20 13:57:11 -0600

    restructure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/hello')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
