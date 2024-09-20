554a0a4fde4c187cd56f62cd55db0b6ed8c17750
Drew Willard <drew@bcalternative.com>
2024-09-20 14:29:45 -0600

    Build production version and update CNAME

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5001/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message));
+    setMessage('Welcome to TorSee!');
   }, []);
 
   return (
